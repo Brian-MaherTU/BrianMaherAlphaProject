78749839f33e99f5b3eeedb42f3493323d8f5c12
Brian-MaherTU <71657390+Brian-MaherTU@users.noreply.github.com>
2020-11-27 12:33:28 +0000

    Project upload

---
+++ unity project Alpha/Assets/Scripts/FinishLine.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+
+    public AudioSource playSound;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        playSound.Play();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
+
---
+++ unity project Alpha/Assets/Scripts/ObstacleMove3.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMove3 : MonoBehaviour
+{
+    public float speed = 2.0f;
+    public float move = 5.0f;
+  
+    private Vector3 start;
+    // Start is called before the first frame update
+    void Start()
+    {
+        start = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v = start;
+        v.x += move * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+    }
+}
